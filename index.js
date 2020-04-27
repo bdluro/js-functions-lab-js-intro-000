a9455a783b899af14982ed012c7c2c543b783956
Brooke Luro <lurob@me.com>
2020-04-27 20:40:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function happyHolidays() {
 }
 
 function happyCustomHolidayTo(holiday, name) {
-  return `Happy ${holiday}, ${name}`
+  return `Happy ${holiday}, ${name}!`
 }
 
 /*function holidayCountdown(holiday, days) {
